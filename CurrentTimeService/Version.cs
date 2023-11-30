13cb8eda1eba0c823eb96bffd2b9200a3c8fa527
ToMSoN28 <t.kowalski999@gmail.com>
2023-11-30 11:08:54 +0000

    change wersion

--- CurrentTimeService/Version.cs
+++ CurrentTimeService/Version.cs
@@ -2,7 +2,7 @@ public static class Version
 {
     private static int major = 1;
     private static int minor = 0;
-    private static int patch = 0;
+    private static int patch = 1;
 
     public static string Current => $"{major}.{minor}.{patch}";
 
